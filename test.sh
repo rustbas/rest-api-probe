1c8d784f482b290291a8ee76f992ce1a45ad9be0
Basyrov Rustam <hrustbas@gmail.com>
2024-10-31 18:10:18 +0300

    add env in test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+set -xe
+
+source .env
+
+curl --request GET \
+--url "https://api.github.com/repos/octocat/Spoon-Knife/issues" \
+--header "Accept: application/vnd.github+json" \
+--header "Authorization: Bearer ${GH_TOKEN}"
